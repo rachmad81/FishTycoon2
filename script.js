6f0574f441c357fa2d3409dc9d6f6294ce473564
Rachmad Nurhidayat <rachmad81man@gmail.com>
2026-06-29 20:53:33 +0700

    awal

--- script.js
+++ script.js
@@ -1,58 +1,47 @@
-let data=[];
+let data = [];
 
-fetch("data.json")
-.then(r=>r.json())
-.then(json=>{
+fetch('data.json')
+    .then((r) => r.json())
+    .then((json) => {
+        data = json;
 
-data=json;
+        let daftar = [];
 
-let daftar=[];
+        data.forEach((d) => {
+            daftar.push(d.a);
+            daftar.push(d.b);
+        });
 
-data.forEach(d=>{
-    daftar.push(d.a);
-    daftar.push(d.b);
-});
+        daftar = [...new Set(daftar)].sort();
 
-daftar=[...new Set(daftar)].sort();
+        const s1 = document.getElementById('input1');
+        const s2 = document.getElementById('input2');
 
-const s1=document.getElementById("input1");
-const s2=document.getElementById("input2");
+        daftar.forEach((nama) => {
+            let o1 = document.createElement('option');
+            o1.value = nama;
+            o1.textContent = nama;
+            s1.appendChild(o1);
 
-daftar.forEach(nama=>{
+            let o2 = document.createElement('option');
+            o2.value = nama;
+            o2.textContent = nama;
+            s2.appendChild(o2);
+        });
+    });
 
-let o1=document.createElement("option");
-o1.value=nama;
-o1.textContent=nama;
-s1.appendChild(o1);
+function cari() {
+    let a = input1.value;
+    let b = input2.value;
 
-let o2=document.createElement("option");
-o2.value=nama;
-o2.textContent=nama;
-s2.appendChild(o2);
+    let hasil = 'Tidak ditemukan';
 
-});
+    for (let d of data) {
+        if ((d.a === a && d.b === b) || (d.a === b && d.b === a)) {
+            hasil = d.hasil;
+            break;
+        }
+    }
 
-});
-
-function cari(){
-
-let a=input1.value;
-let b=input2.value;
-
-let hasil="Tidak ditemukan";
-
-for(let d of data){
-
-if(
-(d.a===a && d.b===b)||
-(d.a===b && d.b===a)
-){
-hasil=d.hasil;
-break;
-}
-
-}
-
-document.getElementById("hasil").innerHTML=hasil;
-
+    document.getElementById('hasil').innerHTML = hasil;
 }
